8ad8513bf9affcee3172037d3d6675bfaf03bcbd
Juan I. Moreno <morcej@gmail.com>
2023-09-20 17:29:41 -0600

    se quita el link de creación de usarios en el login

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -131,7 +131,7 @@ const Login = () => {
           >
             {i18n.t("login.buttons.submit")}
           </Button>
-          <Grid container>
+          {/* <Grid container>
             <Grid item>
               <Link
                 href="#"
@@ -142,7 +142,7 @@ const Login = () => {
                 {i18n.t("login.buttons.register")}
               </Link>
             </Grid>
-          </Grid>
+          </Grid> */}
         </form>
       </div>
       <Box mt={8}>{/* <Copyright /> */}</Box>
